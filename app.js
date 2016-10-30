3aa13bf6b61365948dbc475c617a36f9b675f5b1
Owloid <lee2363@purdue.edu>
2016-10-30 00:50:34 -0400

    Add basic server

--- app.js
+++ app.js
@@ -1,14 +1,30 @@
-var express = require('express');
+// A very basic web server in node.js
+// Stolen from: Node.js for Front-End Developers by Garann Means (p. 9-10)
 
-var app = express();
-app.use(express.static(__dirname + '/public'));
-app.use(express.static(__dirname + '/pages'));
+const path      = require('path');
+const express   = require('express');
+const routes    = require('./routes/index');
+const logger    = require('morgan');
+const app       = express();
+const port      = 8888;
 
-app.get('/', function(request, response, next) {
-  response.send('index.html');
+console.log('Starting server on port ' + port + '...');
+
+// Log the requests
+app.use(logger('dev'));
+
+// Serve public files
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Serve home page
+app.use('/', routes);
+
+// catch 404 and forward to error handler
+app.get('*', function(request, response, next) {
+  response.status(404).send('Page not found');
 });
 
-var port = 8888;
 app.listen(port, function() {
-    console.log('server listening on port ' + port);
+  console.log('Server listening on port ' + port);
+  console.log();
 });
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,19 @@
-var express = require('express');
-var router = express.Router();
+const path      = require('path');
+const express   = require('express');
+const router    = express.Router();
+const pages     = path.join(__dirname, '..', 'pages');
 
 /* GET home page. */
 router.get('/', function(request, response, next) {
-  router.send('index.html');
+  response.sendFile(path.join(pages, 'index.html'));
+});
+
+router.get('/index', function(request, response, next) {
+  response.redirect('/');
+});
+
+router.get('/home', function(request, response, next) {
+  response.redirect('/');
 });
 
 module.exports = router;
