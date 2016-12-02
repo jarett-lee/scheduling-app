8b4c6e9b986bca74b06ae2f7770e5b6b36f58a81
Owloid <jarett.lee.pi@gmail.com>
2016-12-02 10:44:58 -0500

    Add firebase auth

--- public/javascript/script.js
+++ public/javascript/script.js
@@ -1,4 +1,5 @@
 $( function() {
+  /*
   $( "#sortable1" ).sortable({
       connectWith: ".connectedSortable",
       connectWith: ".droppable-hover",
@@ -22,12 +23,38 @@ $( function() {
     connectWith: ".droppable-hover",
   }).disableSelection();
 
-  $("#sortable4").droppable({
+  $("#sortable5").droppable({
     hoverClass: "droppable-hover",
     drop: function(event, ui) {
         var element = ui.draggable.css('position', '');
         $(this).append(element);
-        $(ui.draggable).fadeOut(1000);
+        $(ui.draggable).fadeOut(10);
     }
   });
+  */
+  $( '.connectedSortable' ).sortable({
+    connectWith: '.connectedSortable'
+  });
+
+  var firebaseRef = firebase.database().ref('test');
+  var element = document.querySelector('#mybutton');
+
+  element.addEventListener("click", function(e) {
+    addTime();
+  }, false);
+
+  function addTime() {
+    firebaseRef.once('value').then(function(snapshot) {
+      var previousTime = snapshot.val().time;
+
+      var date = new Date();
+      var time = date.toString();
+
+      firebaseRef.set({
+        previousTime: previousTime,
+        time: time
+      });
+    });
+  }
+
 } );
