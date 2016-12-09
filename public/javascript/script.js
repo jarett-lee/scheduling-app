c3dd79547f2d188c58aa1b73f6ade5f95ee08386
Owloid <jarett.lee.pi@gmail.com>
2016-12-09 18:08:10 -0500

    Add list save and load

--- public/javascript/script.js
+++ public/javascript/script.js
@@ -1,6 +1,5 @@
 $( function() {
-  /*
-  $( "#sortable1" ).sortable({
+  $( "#repeater" ).sortable({
       connectWith: ".connectedSortable",
       connectWith: ".droppable-hover",
       forcePlaceholderSize: false,
@@ -18,12 +17,12 @@ $( function() {
     }
   });
 
-  $( "#sortable2" ).sortable({
+  $( "#set" ).sortable({
     connectWith: ".connectedSortable",
     connectWith: ".droppable-hover",
   }).disableSelection();
 
-  $("#sortable5").droppable({
+  $("#trash").droppable({
     hoverClass: "droppable-hover",
     drop: function(event, ui) {
         var element = ui.draggable.css('position', '');
@@ -31,7 +30,19 @@ $( function() {
         $(ui.draggable).fadeOut(10);
     }
   });
-  */
+
+  var defaultScreen = {
+    repeat:['Event 1'],
+    set:['My event', 'Other event']
+  };
+
+  var emptyScreen = {
+    repeat:[],
+    set:[]
+  };
+
+  var screen = defaultScreen;
+
   $( '.connectedSortable' ).sortable({
     connectWith: '.connectedSortable'
   });
@@ -45,10 +56,25 @@ $( function() {
     if (id) {
       var firebaseUserRef = firebase.database().ref('/testuser/' + id);
 
+      var repeat = [];
+      var set = [];
+
+      var repeatId = document.getElementById('repeat');
+      for (var i = 0; i < repeatId.children.length; i++) {
+        repeat.push(repeatId.children[i].innerHTML);
+      }
+
+      var setId = document.getElementById('set');
+      for (var i = 0; i < setId.children.length; i++) {
+        set.push(setId.children[i].innerHTML);
+      }
+
       var date = new Date();
       var time = date.toString();
 
       firebaseUserRef.set({
+        repeat: repeat,
+        set: set,
         time: time
       });
     }
@@ -56,6 +82,24 @@ $( function() {
 
   function display(info) {
     console.log(info);
+
+    var printer = '';
+    for (var i = 0; i < info.repeat.length; i++) {
+      printer += '<li class="ui-state-default repeater">';
+      printer += info.repeat[i];
+      printer += '</li>';
+      printer += '\n';
+    }
+    $( '#repeat' ).html(printer);
+
+    printer = '';
+    for (var i = 0; i < info.set.length; i++) {
+      printer += '<li class="ui-state-default highlight-1">';
+      printer += info.set[i];
+      printer += '</li>';
+      printer += '\n';
+    }
+    $( '#set' ).html(printer);
   }
 
   $( '#load' ).click(function() {
@@ -68,10 +112,23 @@ $( function() {
       var firebaseUserRef = firebase.database().ref('/testuser/' + id);
 
       firebaseUserRef.once('value').then(function(snapshot) {
-        var info = snapshot.val().time;
-        display(info);
+        var info = snapshot.val();
+        if (info.repeat && info.set) {
+          display(info);
+        }
+        else {
+          newScreen();
+        }
       });
     }
+    else {
+      newScreen();
+    }
+  }
+
+  function newScreen() {
+    screen = defaultScreen;
+    display(screen);
   }
 
   var firebaseRef = firebase.database().ref('test');
